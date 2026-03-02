ac85726696f7d9a526ab11065969564566769afd
shiya121 <zakikukuhpamungkas@gmail.com>
2026-03-02 12:00:00 +0700

    Set up basic routing with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,13 @@
-Not found
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
